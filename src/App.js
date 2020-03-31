4daa920095b45a98ec768638f06f6396726a2e27
Ali Bayatpour <aliglbayatpour@gmail.com>
2020-03-31 23:05:05 +0300

    adding items in checkout page

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import checkoutPage from "./pages/checkout/checkout.component.jsx";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -39,6 +42,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />>
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={checkoutPage} />
           <Route
             exact
             path="/signin"
@@ -52,8 +56,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
