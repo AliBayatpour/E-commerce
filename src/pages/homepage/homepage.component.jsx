5c7be82d1c1fffb1d3d38162f6c85c10e310b814
Ali Bayatpour <aliglbayatpour@gmail.com>
2021-01-19 13:45:25 +0200

    added meta tags

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
+import MetaTags from "react-meta-tags";
 
 const HomePage = () => {
   return (
     <div className="homepage">
+      <MetaTags>
+        <title>Page 1</title>
+        <meta
+          name="description"
+          content="a shopping website created by Ali Bayatpour."
+        />
+        <meta property="og:title" content="Online Shopping" />
+        <meta property="og:image" content="../../assets/og.jpg" />
+      </MetaTags>
       <div className="directory-menu">
         <Directory />
       </div>
